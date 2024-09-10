305d8ec25b9287ce8d15df4bfbd1096d4bbfd4ff
khushn <khushnood.naqvi@gmail.com>
2024-09-10 15:11:37 +0530

    improved minigrep using iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,19 +29,31 @@ fn parse_config(args: &[String]) -> Config {
 
 // improvement (or just a variant) over above
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
+
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // the '?' operator in the end is key, it can return Error (we studied this in an earlier chapter)
@@ -62,6 +74,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+    /*
+    commenting this to use iterator adaptor way
     let mut v = Vec::<&str>::new();
     for line in contents.lines() {
         if line.contains(query) {
@@ -69,10 +84,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         }
     }
     v
+    */
+
+    // iterator adaptor approach prefered in rust over looping (chapter 13.3)
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+    /*
+    commenting this to use iterator adaptor way
     let mut v = Vec::<&str>::new();
     for line in contents.lines() {
         if line.to_lowercase().contains(&query.to_lowercase()) {
@@ -80,6 +105,13 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
         }
     }
     v
+    */
+
+    // iterator adaptor approach prefered in rust over looping (chapter 13.3)
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 #[cfg(test)]
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,12 +3,12 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
     // let cfg = parse_config(&args);
     // let cfg = Config::new(&args);
-    let cfg = Config::build(&args).unwrap_or_else(|err| {
+    let cfg = Config::build(env::args()).unwrap_or_else(|err| {
 
     	// eprintln! macro writes to standard error
         eprintln!("Problem parsing arguments: {err}");
