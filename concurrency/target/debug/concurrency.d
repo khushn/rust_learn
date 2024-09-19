f388c86583ab345bbc63b13f184a4e444b41c833
khushn <khushnood.naqvi@gmail.com>
2024-09-19 18:08:53 +0530

    removed target

--- concurrency/target/debug/concurrency.d
+++
@@ -1,1 +0,0 @@
-/home/khush/workspace/git_repo/rust_learn/concurrency/target/debug/concurrency: /home/khush/workspace/git_repo/rust_learn/concurrency/src/main.rs
--- concurrency/target/debug/deps/concurrency-6cd845462fc173a1.d
+++
@@ -1,5 +0,0 @@
-/home/khush/workspace/git_repo/rust_learn/concurrency/target/debug/deps/concurrency-6cd845462fc173a1: src/main.rs
-
-/home/khush/workspace/git_repo/rust_learn/concurrency/target/debug/deps/concurrency-6cd845462fc173a1.d: src/main.rs
-
-src/main.rs:
