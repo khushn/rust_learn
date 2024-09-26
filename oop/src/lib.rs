a91c33cf130490c89d8ad2897420115d6e0fb7e7
khushn <khushnood.naqvi@gmail.com>
2024-09-26 17:17:22 +0530

    Traits for oop

--- oop/src/lib.rs
+++ oop/src/lib.rs
@@ -1,5 +1,67 @@
 use std::collections::HashSet;
 
+// trait draw
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+/*
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
+}
+
+impl<T> Screen<T>
+where
+    T: Draw,
+{
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+*/
+
+// The above doesn't work, hence commented (it uses the T type of the first val added in the Vec)
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        println!("In Screen run()");
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+
 #[derive(Debug)]
 pub struct AveragedCollection {
     //list: Vec<i32>,
--- oop/src/main.rs
+++ oop/src/main.rs
@@ -1,4 +1,3 @@
-
 use oop::*;
 
 fn main() {
@@ -9,4 +8,27 @@ fn main() {
     println!("ac: {:?}", ac);
     ac.remove(3);
     println!("ac: {:?}", ac);
+
+    // traits
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    //println!("screen: {:?}", screen);
+    screen.run();
 }
