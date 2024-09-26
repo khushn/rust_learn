49e067bf8dd20d6cd7406219bca57cc1490baa89
khushn <khushnood.naqvi@gmail.com>
2024-09-26 15:14:20 +0530

    Mutex and Arc (Atomic reference counting) used in threads

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,7 +1,7 @@
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc;
-
 
 fn main() {
     let handle = thread::spawn(|| {
@@ -17,7 +17,6 @@ fn main() {
     }
 
     handle.join().unwrap();
-
 
     let v = vec![1, 2, 3];
 
@@ -45,7 +44,6 @@ fn main() {
     let gottenval = rx.try_recv().unwrap();
     println!("gottenval: {:?}", gottenval);
 
-
     // sending multiple values in a channel
     println!("\n-------- sending multiple values in a channel -------");
     let (tx, rx) = mpsc::channel();
@@ -63,7 +61,7 @@ fn main() {
 
         for val in vals {
             tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_millis(100));
         }
     });
 
@@ -78,7 +76,7 @@ fn main() {
 
         for val in vals {
             tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_millis(100));
         }
     });
 
@@ -86,4 +84,34 @@ fn main() {
         println!("Got: {received}");
     }
 
+    println!("\n-------- Mutexes and shared state concurrency -------");
+    //mutex
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num += 1;
+    }
+
+    println!("m = {m:?}");
+
+    // multiple threds using a mutex
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+    	let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
