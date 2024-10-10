be2539519b5e67cd584fac0bc3d58358f360977e
khushn <khushnood.naqvi@gmail.com>
2024-10-10 18:52:53 +0530

    19.1 unsafe code. Integration with C code using 'extern'

--- unsafe_rust/src/main.rs
+++ unsafe_rust/src/main.rs
@@ -1,3 +1,37 @@
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // 19.1 unsafe code
+
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        // *r1 = 6;
+        println!("r1 is: {}", *r1);
+
+        *r2 = 7;
+        println!("r2 is: {}", *r2);
+        println!("r1 is: {}", *r1);
+    }
+
+    println!("num is: {}", num);
+
+    let address = 0x012345usize;
+    let r = address as *const i32;
+
+    unsafe fn dangerous() {}
+
+    unsafe {
+        dangerous();
+    }
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
 }
