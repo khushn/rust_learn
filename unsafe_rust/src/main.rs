355f0ca8cfac41cfebbc380e57dcc9f5bb1234ac
khushn <khushnood.naqvi@gmail.com>
2024-10-14 20:05:32 +0530

    Advanced types

--- unsafe_rust/src/main.rs
+++ unsafe_rust/src/main.rs
@@ -48,8 +48,37 @@ fn main() {
     person.fly();
 
     // super trait
-    let p = Point{x: 2, y:3};
+    let p = Point { x: 2, y: 3 };
     p.outline_print();
+
+    // section 19.3 Advanced types
+
+    // also called as newtypes,
+    // basically a type alias to give it meaning to API callers for example
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);
+
+    // to have shorter aliases for long and complex types
+    // e.g in this case a closure function variable which lives through out the entirety of the program
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+
+    let f: Thunk = Box::new(|| println!("hi"));
+
+    fn takes_long_type(f: Thunk) {
+        f();
+    }
+
+    fn returns_long_type() -> Thunk {
+        let f: Box<dyn Fn() + Send + 'static> = Box::new(|| println!("hi"));
+        f
+    }
+
+    let f: Box<dyn Fn() + Send + 'static> = Box::new(|| println!("hi there!!"));
+    takes_long_type(f);
 }
 
 // advaned traits
