af6b1f52dc094b6ec96472808eac182229e06738
khushn <khushnood.naqvi@gmail.com>
2024-10-22 19:42:02 +0530

    checking status line, and if non / showing error page

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,9 +1,8 @@
 use std::{
-	fs,
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
-
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -24,14 +23,29 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
+    println!("first line: {:?}", http_request[0]);
+
     //println!("Request: {http_request:#?}");
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    let req_pattern: String = String::from("GET / ");
+    if http_request[0].contains(&req_pattern) {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+        println!("matched");
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+    	println!("not matched");
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
 
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
