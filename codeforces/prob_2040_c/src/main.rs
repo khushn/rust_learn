5bf42210302ba5285750c08f00c276c1ad91373f
khushn <khushnood.naqvi@gmail.com>
2024-12-20 19:54:30 +0530

    Forgot to checkin the code itself

--- codeforces/prob_2040_c/src/main.rs
+++ codeforces/prob_2040_c/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     for _ in 0..t {
         input.clear();
         io::stdin().read_line(&mut input).unwrap();
-        let numbers: Vec<i32> = input
+        let numbers: Vec<i64> = input
             .trim()
             .split_whitespace()
             .map(|x| x.parse().expect("Invalid input"))
@@ -29,24 +29,29 @@ fn main() {
         // Placeholder for your logic
         //println!("Processing: n = {}, k = {}", n, k);
 
-        let mut total = i32::pow(2, (n - 1).try_into().unwrap());
+        // let mut total: i64 = i32::pow(2, (n - 1) as u32) as i64;
+        let mut total: i64 = n - 1;
         //println!("total: {}", total);
 
-        if k > total {
+        let klog2 = (k as f64).log2();
+
+        if klog2 > total as f64 {
             println!("{:?}", -1);
             continue;
         }
 
-        let mut rem: i32 = k;
+        let mut rem: i64 = k;
         let mut first: usize = 0;
         let mut last: usize = (n - 1) as usize;
-        let mut vec: Vec<i32> = vec![0; n.try_into().unwrap()]; // Vector of size n, initialized to 0
+        let mut vec: Vec<i64> = vec![0; n.try_into().unwrap()]; // Vector of size n, initialized to 0
         for num in 1..n {
-            total = total / 2;
-            if rem > total {
+            total -= 1;
+            let remlog2 = (rem as f64).log2();
+            //println!("remlog2: {:?}, total: {}", remlog2, total);
+            if remlog2 > total as f64 {
                 vec[last] = num;
                 last -= 1;
-                rem -= total;
+                rem -= i64::pow(2, total as u32) as i64;
             } else {
                 vec[first] = num;
                 first += 1;
