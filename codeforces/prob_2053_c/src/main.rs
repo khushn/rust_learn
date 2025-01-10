e5471bbe606d30c9e7864c95ea92f7c31a27fc9b
khushn <khushnood.naqvi@gmail.com>
2025-01-10 16:12:53 +0530

    Write an optimized recursive algo, which just computes the left most branch of the tree, and just projects the rest. It worked!

--- codeforces/prob_2053_c/src/main.rs
+++ codeforces/prob_2053_c/src/main.rs
@@ -42,35 +42,48 @@ fn main() {
     for (n, k) in test_cases {
         //println!("Processing test case: n = {}, k = {}", n, k);
 
-        let lv = star_gaze(1, n, k);
+        let (lv, num) = star_gaze(1, n, k);
         println!("{}", lv);
     }
 }
 
-fn star_gaze(b: u32, e: u32, k: u32) -> u64 {
+
+/*
+Optimized recursive function to invoke only the left most branch of the 
+tree and compute the rest.
+*/
+fn star_gaze(b: u32, e: u32, k: u32) -> (u64, u64) {
     if b > e {
-        return 0;
+        return (0, 0);
     }
     //println!("b: {}, e: {}", b, e);
 
     let l = e - b + 1;
 
     if l < k {
-        return 0;
+        return (0, 0);
     }
 
     if l == 1 {
-    	return b as u64;
+        return (b as u64, 1);
     }
 
     let m = (b + e) / 2;
     if l % 2 == 0 {
-        let v1 = star_gaze(b, m, k);
-        let v2 = star_gaze(m + 1, e, k);
-        v1 + v2
+        let (v1, n1) = star_gaze(b, m, k);
+
+        // instead of invoking for 2nd half we derive using the values of left half
+        // this will solve TLE issue
+        // This works as they are symmetrical we just need to add n1*m to the result
+        //let (v2, n2) = star_gaze(m + 1, e, k);
+        (v1 + v1 + n1 * m as u64, n1 * 2)
     } else {
-        let v1 = star_gaze(b, m - 1, k);
-        let v2 = star_gaze(m + 1, e, k);
-        m as u64 + v1 + v2
+        let (v1, n1) = star_gaze(b, m - 1, k);
+
+        // instead of invoking for 2nd half we derive using the values of left half
+        // this will solve TLE issue
+        // This works as they are symmetrical we just need to add n1*(m+1) to the result
+        // let (v2, n2) = star_gaze(m + 1, e, k);
+        (m as u64 + v1 + v1 + n1 * m as u64, n1 * 2 + 1)
     }
 }
