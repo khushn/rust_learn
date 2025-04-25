ed760b1572f3399ecf53d920f47acced1670f507
khushn <khushnood.naqvi@gmail.com>
2025-04-25 18:53:52 +0530

    Working solution. But may exceed time limit. Need a faster one.

--- cses/trailing_zeros/src/main.rs
+++ cses/trailing_zeros/src/main.rs
@@ -13,39 +13,15 @@ fn main() {
     // Read the number of test cases
     let n: u32 = lines.next().unwrap().unwrap().trim().parse().unwrap();
 
-    // let MODULO: u32 = 1000000000;
-    let MODULO: u32 = 1000;
-
     let mut num_trailing_zeros: u32 = 0;
     let mut mult: u64 = 1;
     for num in 2..=n {
         //println!("processing num: {:?}", num);
         let mut i = num;
-        while (i / 10) * 10 == i {
-            i = i / 10;
+        while i % 5 == 0 {
+            i = i / 5;
             num_trailing_zeros += 1;
         }
-
-        //println!("num_trailing_zeros in {:?} : {:?}", i, num_trailing_zeros);
-
-        i = i % MODULO;
-        //println!("last non zero digit: {:?}", i);
-        mult *= i as u64;
-        if (mult / 10) * 10 == mult {
-            mult /= 10;
-            num_trailing_zeros += 1;
-        }
-
-        /*
-        println!(
-            "num_trailing_zeros after mult {:?} : {:?}",
-            mult, num_trailing_zeros
-        );
-        */
-
-        mult = mult % MODULO as u64;
-        //println!("residual mult: {:?}", mult);
-        //println!("---");
     }
 
     println!("{:?}", num_trailing_zeros);
